e74e6022e8793a731c119c6f1dea7492856e0e86
dialmedu <diegomesa.1414@gmail.com>
2017-06-14 18:36:25 -0500

    Semana #2 Paletas e Implementacion VUE.JS

---
+++ reto-web-design/semana-2/paletas_core.js
@@ -0,0 +1,26 @@
+var paletas = new Vue({
+  el: '#appaletas',
+  data: {
+    message: 'juicy gradients',
+    flavors:[
+    	{ name: 'blueberry',
+    	  colors: [
+    	  	{ name: '#17ead9' },
+    	  	{ name: '#6078ea'}
+    	  ]
+    	},
+    	{ name: 'mint',
+    	  colors: [
+    	  	{ name: '#23bcbb' },
+    	  	{ name: '#45e994'}
+    	  ]
+    	},
+    	{ name: 'blueberry',
+    	  colors: [
+    	  	{ name: '#17ead9' },
+    	  	{ name: '#6078ea'}
+    	  ]
+    	}
+    ]
+  }
+})
