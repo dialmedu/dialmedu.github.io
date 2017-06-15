4c843029e1040037c1acb0506616d14fbb383be8
dialmedu <diegomesa.1414@gmail.com>
2017-06-14 21:33:36 -0500

    Semana #2 Carga con VUE.JS y mejora de UX con mordida

--- reto-web-design/semana-2/paletas_core.js
+++ reto-web-design/semana-2/paletas_core.js
@@ -15,12 +15,60 @@ var paletas = new Vue({
     	  	{ name: '#45e994'}
     	  ]
     	},
-    	{ name: 'blueberry',
+    	{ name: 'Orange',
     	  colors: [
-    	  	{ name: '#17ead9' },
-    	  	{ name: '#6078ea'}
+    	  	{ name: '#FFDEA7' },
+    	  	{ name: '#EA6362'}
     	  ]
-    	}
+    	},
+        { name: 'Pomegranate',
+          colors: [
+            { name: '#9B3CB7' },
+            { name: '#FF396F'}
+          ]
+        },
+        { name: 'Green Tea',
+          colors: [
+            { name: '#004B91' },
+            { name: '#78CC37'}
+          ]
+        },
+        { name: 'Blackberry',
+          colors: [
+            { name: '#38B8F2' },
+            { name: '#843CF7'}
+          ]
+        },
+        { name: 'Plum',
+          colors: [
+            { name: '#e35C67' },
+            { name: '#381CE2'}
+          ]
+        },
+        { name: 'Grape',
+          colors: [
+            { name: '#EFBFD5' },
+            { name: '#9D61FD'}
+          ]
+        },
+        { name: 'Lime',
+          colors: [
+            { name: '#45E0A7' },
+            { name: '#D5E969'}
+          ]
+        },
+        { name: 'Peach',
+          colors: [
+            { name: '#D73E68' },
+            { name: '#FBC79A'}
+          ]
+        },
+        { name: 'Passion Fruit',
+          colors: [
+            { name: '#8137F7' },
+            { name: '#F6AB3E'}
+          ]
+        }
     ]
   }
 })
